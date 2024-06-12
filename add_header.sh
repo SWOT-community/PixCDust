3f59fb1bd4e72b5ec83ebcc02d5c03a91372d5a5
zawadzl <lionel.zawadzki@cnes.fr>
2024-06-12 08:52:24 +0200

    cleaning useless files

--- add_header.sh
+++
@@ -1,8 +0,0 @@
-
-header=$1
-
-for pathname in `find pixcdust/tools -name *py` ; do
-    mv $pathname "$pathname.tmp"
-    { cat -- "$header"; printf '%s\n\n'; cat -- "$pathname.tmp"; } >"$pathname"
-    rm "$pathname.tmp"
-done
