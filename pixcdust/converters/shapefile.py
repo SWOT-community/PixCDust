9cee72162edf74f588eb89c2189e4621c426cfc8
guillaume <guillaume.beauchamp@cs-soprasteria.com>
2024-10-29 18:13:04 +0100

    fix shapefile aoe handling and directory creation consistency

--- pixcdust/converters/shapefile.py
+++ pixcdust/converters/shapefile.py
@@ -13,9 +13,12 @@ class PixCNc2ShpConverter(PixCConverter):
         """function to create a database from a single or multiple\
             netcdf PIXC files
         """
-
+        try:
+            os.mkdir(self.path_out)
+        except FileExistsError:
+            pass
         for path in tqdm(self.path_in):
-            ncsimple = PixCNcSimpleReader(path, self.variables)
+            ncsimple = PixCNcSimpleReader(path, self.variables, area_of_interest=self.area_of_interest)
 
             filename_out = os.path.splitext(os.path.basename(path))[0]
             path_out = os.path.join(
@@ -28,9 +31,7 @@ class PixCNc2ShpConverter(PixCConverter):
 
             # converting data from xarray to geodataframe
             ncsimple.open_dataset()
-            gdf = ncsimple.to_geodataframe(
-                area_of_interest=self.area_of_interest
-            )
+            gdf = ncsimple.to_geodataframe()
 
             if gdf.size == 0:
                 tqdm.write(
